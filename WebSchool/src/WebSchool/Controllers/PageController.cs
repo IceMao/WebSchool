95a9366bb95bcd0d5dd5ed6923dee075670235cb
Cream2015 <343224963@qq.com>
2015-12-18 23:46:35 +0800

    网站风格修改

--- WebSchool/src/WebSchool/Controllers/PageController.cs
+++ WebSchool/src/WebSchool/Controllers/PageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using WebSchool.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +12,8 @@ namespace WebSchool.Controllers
     public class PageController : Controller
     {
         // GET: /<controller>/
+        [FromServices]
+        public ArticleContext DB { get; set; }
         [HttpGet]
         public IActionResult News()
         {
@@ -38,7 +41,8 @@ namespace WebSchool.Controllers
         [HttpGet]
         public IActionResult RecruitStudents()
         {
-            return View();
+            var ret = DB.RecruitStudents.ToList();
+            return View(ret);
         }
 
         [HttpGet]
